f63fa7731b183b3446197a707658470af60f8471
El-Ghoul <77440128+El-Ghoul@users.noreply.github.com>
2021-01-21 14:07:30 +0200

    Add files via upload

---
+++ py.py
@@ -0,0 +1,50 @@
+
+
+while True:
+
+    x = input()
+
+    if 'how are you' or 'how are you?' or 'how are you doing' in x:
+         print('Good and you?')
+
+
+    else:
+        print("I don't understand ")
+
+      
+
+        if 'fine thank you' in x:
+            print('awesome!')
+
+ #   break
+
+while True:
+
+    y = input()
+
+    if "who is amir?" in x:
+        print("He's my creator")
+
+
+while True:
+
+    print(input())
+
+
+
+
+
+
+ #   if 'im fine thank you' in y:
+  #      print('awesome!')
+
+
+
+
+
+
+
+
+
+
+
